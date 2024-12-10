bab150c7767020a102e4b214ddef95307523cd03
Cz4rNy213 <mikolaj.kolek2@gmail.com>
2024-12-10 19:49:22 +0100

    project 16 done

--- Assembler_16/main.asm
+++ Assembler_16/main.asm
@@ -23,7 +23,21 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
+main:
+			mov.b #00010000b, R4
+			bit.b #10000000b, R4			; check bit 7, if it is 1 then Z flag = 0, otherwise Z flag = 1
+			bit.b #00010000b, R4
 
+			mov.b #99, R5
+			cmp.b #99, R5					; check if value in R5 is equal 99 then Z flag = 1
+			cmp.b #77, R5
+
+			mov.b #-99, R6
+			tst.b R6						; tst substract 0 from R6
+
+
+
+			jmp main
                                             
 
 ;-------------------------------------------------------------------------------
